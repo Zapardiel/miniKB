40d05a00b922889c1046b0ac0f87eb83a2e1e5de
Pablo Zapardiel <pablozapardiel@gmail.com>
2017-03-06 23:45:58 +0100

    Segundo Commit Utilizando Flags para que lo entienda el EditorInfo

--- app/src/main/java/com/honeywell/minikb/minikb.java
+++ app/src/main/java/com/honeywell/minikb/minikb.java
@@ -5,9 +5,12 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.os.SystemClock;
+import android.view.InputDevice;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import android.widget.Toast;
 
 public class minikb extends InputMethodService
         implements KeyboardView.OnKeyboardActionListener{
@@ -60,7 +63,8 @@ public class minikb extends InputMethodService
         playClick(primaryCode);
         switch (primaryCode){
             case Keyboard.KEYCODE_DELETE:
-                ic.deleteSurroundingText(1, 0);
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
                 break;
             case Keyboard.KEYCODE_SHIFT:
                 caps = !caps;
@@ -68,7 +72,29 @@ public class minikb extends InputMethodService
                 kv.invalidateAllKeys();
                 break;
             case Keyboard.KEYCODE_DONE:
-                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                Toast.makeText(this, "Enter", Toast.LENGTH_SHORT).show();
+                ic.sendKeyEvent(new KeyEvent(
+                        SystemClock.uptimeMillis(), SystemClock.uptimeMillis(),
+                        KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER,
+                        0,
+                        0,
+                        0,
+                        0,
+                        KeyEvent.FLAG_SOFT_KEYBOARD | KeyEvent.FLAG_KEEP_TOUCH_MODE | KeyEvent.FLAG_EDITOR_ACTION,
+                        InputDevice.SOURCE_KEYBOARD));
+                ic.sendKeyEvent(new KeyEvent(
+                        SystemClock.uptimeMillis(), SystemClock.uptimeMillis(),
+                        KeyEvent.ACTION_UP, KeyEvent.KEYCODE_ENTER,
+                        0,
+                        0,
+                        0,
+                        0,
+                        KeyEvent.FLAG_SOFT_KEYBOARD | KeyEvent.FLAG_KEEP_TOUCH_MODE | KeyEvent.FLAG_EDITOR_ACTION,
+                        InputDevice.SOURCE_KEYBOARD));
+                break;
+            case 131:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_F1));
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_F1));
                 break;
             default:
                 char code = (char) primaryCode;
